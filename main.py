f4d931fa1ba952bdd61dcb2dfc8f6cc2250e5edd
ninos77 <ninos_zaiya@hotmail.com>
2021-12-25 13:36:29 +0100

    Added Pandas library,added func. to create id

--- main.py
+++ main.py
@@ -1,9 +1,46 @@
-class User:
+
+class User():
     def __init__(self,id,name, email):
         self.id = id
         self.name = name
         self.email = email
-    
-    
+
 
 import csv
+
+#We have to install Pandas--> https://pandas.pydata.org/docs/getting_started/install.html
+import pandas as pd
+
+
+#to check if users have entered an integer
+def check_user_input_int(question):
+    while True:
+        try:
+            int_input = int(input(question))
+            break
+        except ValueError:
+            print('You have to enter an integer')
+            pass
+        
+    return int_input
+
+#to check if users have entered string not empty 
+def check_user_input_str(question):
+    while True:
+        str_input = input(question)
+        if str_input and str_input.strip():
+            break
+        else:
+            print('Your input is empty, you have to enter somthing ')
+            pass   
+    return str_input
+
+#create ID for each post when users write in guestbook
+def generate_id():
+    df = pd.read_csv('data.csv', delimiter=',')
+    list_of_rows = [list(row) for row in df.values]
+    user_id = len(list_of_rows) + 1  
+    for row in list_of_rows:
+        if user_id == row[0]:
+            user_id = list_of_rows[-1][0]+1
+    return user_id
